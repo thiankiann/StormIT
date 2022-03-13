f07826f8811baff1b5cc911ef643ee8e9f35e41d
@thiankiann <thiankiann@gmail.com>
2022-03-13 13:17:27 +0000

    first commit

---
+++ src/main/java/pl/stormit/demoProjectStormIT/DemoProjectStormItApplication.java
@@ -0,0 +1,13 @@
+package pl.stormit.demoProjectStormIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectStormItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectStormItApplication.class, args);
+	}
+
+}
